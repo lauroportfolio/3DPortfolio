24342f57697d8ef9be08558283a9a5bffa298ded
lauroportfolio <lauroportfolio@gmail.com>
2023-09-26 17:34:16 -0300

    first commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
+import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 
 import { styles } from '../styles'
@@ -6,65 +7,70 @@ import { navLinks } from '../constants'
 import { logo, menu, close } from '../assets'
 
 const Navbar = () => {
-  const [active, setActive] = useState("")
+  const [active, setActive] = useState('')
   const [toggle, setToggle] = useState(false)
 
   return (
     <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
-      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
-        <Link 
-          to="/" 
-          className="flex items-center gap-2" 
+      <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
+        <Link
+          to="/"
+          className="flex items-center gap-2"
           onClick={() => {
             setActive("")
             window.scrollTo(0, 0)
           }}
         >
-          <img src={logo} alt='Logo' className="w-9 h-9 object-contain" />
-          <p className="text-white text-[18px] font-bold cursor-pointer flex">
-            Lauro Flores&nbsp;
-            <span className="sm:block hidden">| Portfólio</span>
+          <img
+            src={logo}
+            alt="logo"
+            className="w-9 h-9 object-contain"  
+          />
+          <p className='text-white font-bold text-[18px] cursor-pointer flex'>
+            Lauro &nbsp;
+            <span className='sm:block hidden'>| F. F. Taipina</span>
           </p>
         </Link>
-        <ul className="list-none hidden sm:flex flex-row gap-10">
-          {navLinks.map((Link) => (
-            <li 
-              key={Link.id} 
+        <ul className='list-none hidden sm:flex flex-row gap-10'>
+          {navLinks.map((link) => (
+            <li
+              key={link.id}
               className={`${
-                active === Link.title
+                active === link.title
                   ? "text-white"
                   : "text-secondary"
               } hover:text-white text-[18px] font-medium cursor-pointer`}
-              onClick={() => setActive(Link.title)}
+              onClick={() => setActive(link.title)}
             >
-              <a href={`#${Link.id}`}>{Link.title}</a>
+              <a href={`#${link.id}`}>{link.title}</a>
             </li>
           ))}
         </ul>
-
-        <div className="sm:hidden flex flex-1 justify-end items-center">
+        
+        <div className='sm:hidden flex flex-1 justify-end items-center'>
           <img 
-            src={toggle ? close : menu} 
-            alt="Menu"
-            className="w-[28px] h-[28px] object-contain cursor-pointer"
+            src={toggle ? close : menu}
+            alt="menu"
+            className='w-[28px] h-[28px] object-contain cursor-pointer'
             onClick={() => setToggle(!toggle)}
           />
-          <div className={`${!toggle ? 'hidden' : 'flex'} p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w[140px] z-10 rounded-xl`}>
-            <ul className="list-none flex justify-end items-start flex-col gap-4">
-              {navLinks.map((Link) => (
-                <li 
-                  key={Link.id} 
+
+          <div className={`${!toggle ? 'hidden' : 'flex'} p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}>
+            <ul className='list-none flex justify-end items-start flex-col gap-4'>
+              {navLinks.map((link) => (
+                <li
+                  key={link.id}
                   className={`${
-                    active === Link.title
+                    active === link.title
                       ? "text-white"
                       : "text-secondary"
-                  } font-poppins font-medium cursor-pointer text-[16px]`}
+                  } font-poppins font-medium cursor-pointer text-[16px] hover:text-white`}
                   onClick={() => {
                     setToggle(!toggle)
-                    setActive(Link.title)}
-                  }
+                    setActive(link.title)
+                  }}
                 >
-                  <a href={`#${Link.id}`}>{Link.title}</a>
+                  <a href={`#${link.id}`}>{link.title}</a>
                 </li>
               ))}
             </ul>
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,20 +1,28 @@
-import { Suspense, useEffect, useState } from 'react'
 import { Canvas } from '@react-three/fiber'
+import { Suspense, useEffect, useState } from 'react'
 import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
 
 import CanvasLoader from '../Loader'
 
-const Computers = () => {
+const Computers = ({ isMobile }) => {
   const computer = useGLTF("./desktop_pc/scene.gltf")
 
   return (
     <mesh>
       <hemisphereLight intensity={0.15} groundColor='black' />
       <pointLight intensity={1} />
+      <spotLight
+        position={[-20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}
+      />
       <primitive
         object={computer.scene}
-        scale={0.75}
-        position={[0, -3.25, -1.5]}
+        scale={isMobile ? 0.7 : 0.75}
+        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
@@ -22,6 +30,29 @@ const Computers = () => {
 }
 
 const ComputersCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false)
+
+  useEffect(() => {
+    // Add a Listener for changes to the screen size
+    const mediaQuery = window.matchMedia('(max-width: 500px)')
+
+    // Set the initial value of the 'isMobile state variable
+    setIsMobile(mediaQuery.matches)
+
+    // Define a callback function to handle changes to the media query
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches)
+    }
+
+    // Add the callback function as a Listener for changes to the media query
+    mediaQuery.addEventListener('change', handleMediaQueryChange)
+
+    // Remove the Listener when the component is unmounted
+    return () => {
+      mediaQuery.removeEventListener('change', handleMediaQueryChange)
+    }
+  }, [])
+
   return (
     <Canvas
       frameloop="demand"
@@ -35,7 +66,7 @@ const ComputersCanvas = () => {
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
-        <Computers />
+        <Computers isMobile={isMobile} />
       </Suspense>
 
       <Preload all />
